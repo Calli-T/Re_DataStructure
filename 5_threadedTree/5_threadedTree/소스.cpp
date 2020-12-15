608ce44627bd9c64d56d28f50894929017475850
Calli-T <joy14479@gmail.com>
2020-12-15 14:59:27 +0900

    커밋

--- 5_threadedTree/5_threadedTree/소스.cpp
+++ 5_threadedTree/5_threadedTree/소스.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-typedef thread_tree* ttp;//threaded tree pointer
+typedef struct thread_tree* ttp;//threaded tree pointer
 struct thread_tree {
 	bool left_thread;
 	ttp left_child;
@@ -33,5 +33,29 @@ void inorderT(ttp tree) {
 		if (temp == tree) break;
 		printf("%3c", temp->data);
 	}
+
+}
+
+//----------------------------------------------------------------------//
+
+ttp makeNode() {
+
+	ttp temp = (ttp)malloc(sizeof(struct thread_tree));
+
+	temp->data = '\0';
+	temp->right_child = 0;
+	temp->left_child = 0;
+	temp->left_thread = false;
+	temp->right_thread = false;
+
+	return temp;
+
+}
+
+//----------------------------------------------------------------------//
+
+int main() {
+
+	return 0;
 
 }
